b1e9e4107d52d228b6bfc720eda17c0a1b9760fa
RodriiAndino <andinorodrigo03@gmail.com>
2023-03-15 11:22:59 -0300

    add: download

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const d = document
-// const $btnDownload = d.getElementById('download')
+const $btnDownload = d.getElementById('download')
 const $btnClean = d.getElementById('clean')
 const $rotateOptions = d.querySelectorAll('#rotate button')
 const $filterOptions = d.querySelectorAll('#filter button')
@@ -106,7 +106,41 @@ const changeFilter = () => {
   applyFilter()
 }
 
+const download = () => {
+  const canvas = document.createElement('canvas')
+  const ctx = canvas.getContext('2d')
+
+  // Le doy al canvas el tamaño de la img
+  canvas.width = $imgPreview.naturalWidth
+  canvas.height = $imgPreview.naturalHeight
+
+  // Le añado al lienzo los efecto
+  ctx.filter = `brightness(${brightness}%) saturate(${saturation}%) invert(${inversion}%) grayscale(${grayscale}%)`
+
+  // muevo el canvas al centro del lienzo
+  ctx.translate(canvas.width / 2, canvas.height / 2)
+
+  if (rotate !== 0) {
+    ctx.rotate((rotate * Math.PI) / 180)
+  }
+
+  // dibujo laimagen dentro del canvas,
+  ctx.drawImage(
+    $imgPreview, // imagen
+    -canvas.width / 2, // le quieto el sobrante del ancho
+    -canvas.height / 2, // le quieto el sobrante del alto
+    canvas.width, // Le asigno el tamaño de la imagen
+    canvas.height
+  )
+
+  const link = document.createElement('a')
+  link.download = 'image.jpg'
+  link.href = canvas.toDataURL()
+  link.click()
+}
+
 $btnClean.addEventListener('click', init)
 $filterSlider.addEventListener('input', changeFilter)
 $input.addEventListener('change', load)
 $change.addEventListener('click', () => $input.click())
+$btnDownload.addEventListener('click', download)
